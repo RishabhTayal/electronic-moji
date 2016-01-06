8116741a4c2107402a4a3c767cc0a7c26c092575
Boris Bügling <boris@icculus.org>
2016-01-06 19:34:22 +0100

    Put first search result into pasteboard

--- Sources/electronic-moji/main.swift
+++ Sources/electronic-moji/main.swift
@@ -2,6 +2,17 @@ import AppKit
 import Commander
 import Emoji
 
+func fillPasteboard(contents: String) {
+	let pboard = NSPasteboard.generalPasteboard()
+	pboard.clearContents()
+	pboard.setData(contents.dataUsingEncoding(NSUTF8StringEncoding), forType: NSPasteboardTypeString)
+}
+
 command { (arg: String) in
-	print(EMOJI.findUnicodeName(arg).map { "\($0)" }.joinWithSeparator("  "))
+	let matchStrings = EMOJI.findUnicodeName(arg).map { "\($0)" }
+	print(matchStrings.joinWithSeparator("  "))
+
+	if let string = matchStrings.first {
+		fillPasteboard(string)
+	}
 }.run()
